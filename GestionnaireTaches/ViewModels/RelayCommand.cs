f4f7a9323334b0d55e5746f36dee20bf9edd2daf
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-04-21 12:53:41 -0400

    feat: Add RelayCommand and task-add logic to ViewModel
    Introduce RelayCommand implementing ICommand (uses Predicate<object> and Action<object> and hooks CommandManager.RequerySuggested). Update TachesViewModel to use ObservableCollection<string> for Taches, expose NouveauTache, add AjouterTache method with validation and formatted add, and wire AjouterTacheCommande to the new RelayCommand in the constructor.

---
+++ GestionnaireTaches/ViewModels/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+
+namespace GestionnaireTaches.ViewModels
+{
+    class RelayCommand: ICommand
+    {
+        // Predicate = fonction qui retourne bool (la condition d'activation)
+        private readonly Predicate<object> _canExecute;
+
+        // Action = ce qui s'exécute au clic (reçoit un paramètre object)
+        private readonly Action<object> _execute;
+
+        public RelayCommand(Predicate<object> canExecute, Action<object> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute(parameter);
+        public void Execute(object parameter) => _execute(parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
--- GestionnaireTaches/ViewModels/TachesViewModel.cs
+++ GestionnaireTaches/ViewModels/TachesViewModel.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
 
 namespace GestionnaireTaches.ViewModels
 {
     class TachesViewModel
     {
+        public Models.NouveauTache NouveauTache { get; set; }
+        public ObservableCollection<string> Taches { get; set; }
+        public ICommand AjouterTacheCommande { get; set; }
+
+        // the function to adda new task
+        public void AjouterTache()
+        {
+            NouveauTache.Erreur = "";
+
+            // verify the task name if null or empty
+            if (string.IsNullOrWhiteSpace(NouveauTache.Nom))
+            {
+                NouveauTache.Erreur = "Le nom est obligatoire.";
+                return;
+            }
+
+            Taches.Add($"{NouveauTache.Icone()} [{NouveauTache.Priorite}] {NouveauTache.Nom}");
+            NouveauTache.Nom = "";
+        }
+
+        public TachesViewModel()
+        {
+            NouveauTache = new Models.NouveauTache();
+            Taches = new ObservableCollection<string>();
+
+            AjouterTacheCommande = new RelayCommand(o => NouveauTache.IsValid, o => AjouterTache());
+        }
     }
 }
